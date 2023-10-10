bb58f5df853dd42fb8140ab41abf2a9554ea43b6
SuperCoder <propscode254@gmail.com>
2023-10-10 21:28:01 +0200

    add arrow

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,14 @@
-const hamburgerInput = document.querySelector('[data-js="hamburger-input"]');
+const hamburger = document.querySelector('[data-js="hamburger-input"]');
 const navList = document.querySelector('[data-js="nav-list"]');
+const scroll = document.querySelector('[data-js="scrollToTop"]');
 
-hamburgerInput.addEventListener("click", () => {
+hamburger.addEventListener("click", () => {
   navList.classList.toggle("display-block");
+});
+
+scroll.addEventListener("change", () => {
+  if (scroll.checked) {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    scroll.checked = false;
+  }
 });
